d28fb4fa0f95a6a821b3f74b8657db38e7a151b2
engrcollins <akinbamicollins@gmail.com>
2020-05-26 23:58:03 +0000

    Updates

---
+++ precache-manifest.b01070edaf78af6c5922a026c4c2f1c3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1e0f2c32c5b0df107e984979361c38",
+    "url": "/woke_lib_client/index.html"
+  },
+  {
+    "revision": "e5d2e48731cd337bbbf2",
+    "url": "/woke_lib_client/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "65d84977e2a219ae8b0b",
+    "url": "/woke_lib_client/static/css/main.6e54a968.chunk.css"
+  },
+  {
+    "revision": "e5d2e48731cd337bbbf2",
+    "url": "/woke_lib_client/static/js/2.21a199c3.chunk.js"
+  },
+  {
+    "revision": "8a9a2ae4a25f202f52818f8f8074424e",
+    "url": "/woke_lib_client/static/js/2.21a199c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d84977e2a219ae8b0b",
+    "url": "/woke_lib_client/static/js/main.308e217b.chunk.js"
+  },
+  {
+    "revision": "e8960a9900bd760cfca5",
+    "url": "/woke_lib_client/static/js/runtime-main.ce7301fc.js"
+  },
+  {
+    "revision": "16f22597f61c6fc090906c4493fc05b3",
+    "url": "/woke_lib_client/static/media/collins.16f22597.jpg"
+  },
+  {
+    "revision": "44dd47b08da36dca402ede0c170ec59d",
+    "url": "/woke_lib_client/static/media/header2.44dd47b0.png"
+  }
+]);
