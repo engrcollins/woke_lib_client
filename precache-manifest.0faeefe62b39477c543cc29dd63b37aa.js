49a5e38b3d75a53bda07ab04621f111fb0ebadd4
engrcollins <akinbamicollins@gmail.com>
2020-05-27 00:03:31 +0000

    Updates

---
+++ precache-manifest.0faeefe62b39477c543cc29dd63b37aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b650437b71c745ab5e557ae35b4d3974",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5d2e48731cd337bbbf2",
+    "url": "./static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "f13dd89de80be8bae830",
+    "url": "./static/css/main.6e54a968.chunk.css"
+  },
+  {
+    "revision": "e5d2e48731cd337bbbf2",
+    "url": "./static/js/2.21a199c3.chunk.js"
+  },
+  {
+    "revision": "8a9a2ae4a25f202f52818f8f8074424e",
+    "url": "./static/js/2.21a199c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13dd89de80be8bae830",
+    "url": "./static/js/main.a90f99b7.chunk.js"
+  },
+  {
+    "revision": "fb830977d962a6960bab",
+    "url": "./static/js/runtime-main.913b19e0.js"
+  },
+  {
+    "revision": "16f22597f61c6fc090906c4493fc05b3",
+    "url": "./static/media/collins.16f22597.jpg"
+  },
+  {
+    "revision": "44dd47b08da36dca402ede0c170ec59d",
+    "url": "./static/media/header2.44dd47b0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/woke_lib_client/precache-manifest.b01070edaf78af6c5922a026c4c2f1c3.js"
+  "./precache-manifest.0faeefe62b39477c543cc29dd63b37aa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/woke_lib_client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
